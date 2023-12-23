f4e8c2b9aefa03e3a03fa54350605c4d411db899
Ayda <aydaasghari9481@gmail.com>
2023-12-23 23:21:06 +0330

    method for creating initial population created

--- src/Genetic.java
+++ src/Genetic.java
@@ -1,7 +1,9 @@
+import java.util.*;
+
 public class Genetic {
     //attributes
-    int N, numberOfGenerations, populationSize;
-    double recombinationRate, mutationRate;
+    static int N, numberOfGenerations, populationSize;
+    static double recombinationRate, mutationRate;
 
     public Genetic(int n, int numberOfGenerations, int populationSize, double recombinationRate, double mutationRate) {
         N = n;
@@ -14,8 +16,26 @@ public class Genetic {
 
 
     //this function randomly generates initial population
-    private String create_initial_population(){
-        String[] initialPopulation = new String[populationSize];
-        return "";
+    private static ArrayList<String> create_initial_population(){
+
+        //in this array we save the chromosomes of first population
+        ArrayList<String> initial_population = new ArrayList<>();
+
+        Random rand = new Random();
+
+        //generating chromosomes
+        for (int i = 0; i < populationSize; i++) {
+            String chromosome = "";
+            for (int j = 0; j < N; j++) {
+                int number = rand.nextInt(N);
+                if (!chromosome.contains(String.valueOf(number)))
+                    chromosome += number;
+                else
+                    j--;
+            }
+            initial_population.add(chromosome);
+        }
+        return initial_population;
     }
+
 }
